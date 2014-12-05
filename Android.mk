a412d296381bd42d8816b808aa4abfc72b15fd70
Mingwei Shi <mingwei.shi@intel.com>
2014-12-06 04:50:11 +0800

    Implement zram memtrack base on kernel Pswap patch
    Tracked-On: GMINL-4499
    
    Pay attention, this patch should work with the kernel patch
    #/c/316675/
    
    Change-Id: I8e7836c762d2d198986c977712506cded8c5db6d
    Signed-off-by: Mingwei Shi <mingwei.shi@intel.com>

--- Android.mk
+++ Android.mk
@@ -21,6 +21,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_C_INCLUDES += hardware/libhardware/include
 LOCAL_SHARED_LIBRARIES := liblog libcutils
-LOCAL_SRC_FILES := memtrack_intel.c gen.c
+LOCAL_SRC_FILES := memtrack_intel.c gen.c zram.c
 LOCAL_MODULE := memtrack.$(TARGET_BOARD_PLATFORM)
 include $(BUILD_SHARED_LIBRARY)
