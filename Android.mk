bd7f94168239de0740fbf3bb12e166588a63c702
Beiyuan Hu <beiyuan.hu@intel.com>
2016-07-15 09:39:36 +0800

    Use memtrack module mali for goldsand
    Change-Id: Icf0927fa679812e09f148f839cb846c6b3cee5fd
    Tracked-On: OAM-29907
    Reviewed-on: 523405

--- Android.mk
+++ Android.mk
@@ -1,6 +1,9 @@
 ifneq ($(filter sofia3gr, $(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles, mali)
 else
+ifneq ($(filter gsd, $(TARGET_BOARD_PLATFORM)),)
+    include $(call all-named-subdir-makefiles, mali)
+else
 ifneq ($(filter sofia_lte, $(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles, mali-midgard)
 else
