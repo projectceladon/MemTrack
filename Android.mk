9a1aea32869efc1452fbc78b79aa61125c948018
Beiyuan Hu <beiyuan.hu@intel.com>
2016-07-22 09:43:42 +0800

    Fix an invalid syntax issue of building.
    A invalid syntax issue was triggerd when building with “make” instead of the Android N replacements of kati and ninja. Fix this issue.
    
    Change-Id: I9313ef3a8119d7c68972dfc0073aa76f983ed3b6
    Tracked-On: OAM-35924
    Reviewed-on: 525263

--- Android.mk
+++ Android.mk
@@ -1,12 +1,9 @@
 ifneq ($(filter sofia3gr, $(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles, mali)
-else
-ifneq ($(filter gsd, $(TARGET_BOARD_PLATFORM)),)
+else ifneq ($(filter gsd, $(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles, mali)
-else
-ifneq ($(filter sofia_lte, $(TARGET_BOARD_PLATFORM)),)
+else ifneq ($(filter sofia_lte, $(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles, mali-midgard)
 else
     include $(call all-named-subdir-makefiles, gen)
-endif
 endif
